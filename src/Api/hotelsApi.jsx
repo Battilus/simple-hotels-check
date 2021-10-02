dc09e69acf64c0c6a1ecb1d579362f589fc85ed7
Battilus <battilus@hotmail.com>
2021-10-02 22:09:35 +0300

    yet anoter fix

--- src/Api/hotelsApi.jsx
+++ src/Api/hotelsApi.jsx
@@ -1,4 +1,4 @@
 
-export const fetchHotels = () => 'http://engine.hotellook.com/api/v2/cache.json'
+export const fetchHotels = () => 'https://engine.hotellook.com/api/v2/cache.json'
 export const fetchHotelsPhotosID = () => 'https://yasen.hotellook.com/photos/hotel_photos'
 export const fetchHotelsPhotos = (hotel_ID) => 'https://photo.hotellook.com/image_v2/limit/'+hotel_ID+'/520/520.auto'
